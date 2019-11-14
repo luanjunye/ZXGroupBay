4441464a02047d3235326a5cf04cbb25a0cc9fe4
Muse丶 <554953278@qq.com>
2019-11-14 10:16:24 +0800

    创建项目

---
+++ config/url.js
@@ -0,0 +1,9 @@
+ const API_BASE_URL = 'http://192.168.0.176:8081/freeterapi/';
+//const API_BASE_URL = 'https://shop.zexuanshipin.com/freeterapi/';
+
+
+module.exports = {
+  //首页
+  IndexUrlBanner: API_BASE_URL + 'index/bannerlist', //首页banner图
+
+};
---
+++ utils/util.js
@@ -0,0 +1,159 @@
+var api = require('../config/url.js');
+
+function formatTime(date) {
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var day = date.getDate()
+
+  var hour = date.getHours()
+  var minute = date.getMinutes()
+  var second = date.getSeconds()
+
+
+  return [year, month, day].map(formatNumber).join('-') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+function formatNumber(n) {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+
+/**
+ * 封封微信的的request
+ */
+function request(url, data = {}, method, header = "application/json") {
+  wx.showLoading({
+    title: '加载中...',
+  });
+  return new Promise(function (resolve, reject) {
+    wx.request({
+      url: url,
+      data: data,
+      method: method,
+      header: {
+        'Content-Type': header,
+        'X-Nideshop-Token': wx.getStorageSync('token')
+      },
+      success: function (res) {
+        wx.hideLoading();
+        if (res.statusCode == 200) {
+
+          if (res.data.errno == 401) {
+            wx.navigateTo({
+              url: '/pages/auth/btnAuth/btnAuth',
+            })
+          } else {
+            resolve(res.data);
+          }
+        } else {
+          reject(res.errMsg);
+        }
+
+      },
+      fail: function (err) {
+        reject(err)
+      }
+    })
+  });
+}
+
+/**
+ * 检查微信会话是否过期
+ */
+function checkSession() {
+  return new Promise(function (resolve, reject) {
+    wx.checkSession({
+      success: function () {
+        resolve(true);
+      },
+      fail: function () {
+        reject(false);
+      }
+    })
+  });
+}
+
+/**
+ * 调用微信登录
+ */
+function login() {
+  return new Promise(function (resolve, reject) {
+    wx.login({
+      success: function (res) {
+        if (res.code) {
+          resolve(res);
+        } else {
+          reject(res);
+        }
+      },
+      fail: function (err) {
+        reject(err);
+      }
+    });
+  });
+}
+
+function redirect(url) {
+
+  //判断页面是否需要登录
+  if (false) {
+    wx.redirectTo({
+      url: '/pages/auth/login/login'
+    });
+    return false;
+  } else {
+    wx.redirectTo({
+      url: url
+    });
+  }
+}
+
+function showErrorToast(msg) {
+  wx.showToast({
+    title: msg,
+    image: '/static/images/icon_error.png'
+  })
+}
+
+function showSuccessToast(msg) {
+  wx.showToast({
+    title: msg,
+  })
+}
+
+function formateDate(date, formatString) {
+  if (!formatString) {
+    formatString = 'yyyy-MM-dd hh:mm:ss';
+  }
+  let o = {
+    "M+": date.getMonth() + 1,                      //月份
+    "d+": date.getDate(),                           //日
+    "h+": date.getHours(),                          //小时
+    "m+": date.getMinutes(),                        //分
+    "s+": date.getSeconds(),                        //秒
+    "q+": Math.floor((date.getMonth() + 3) / 3),    //季度
+    "S": date.getMilliseconds()                     //毫秒
+  };
+  if (/(y+)/.test(formatString)) {
+    formatString = formatString.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+  }
+  for (let k in o) {
+    if (new RegExp("(" + k + ")").test(formatString)) {
+      formatString = formatString.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    }
+  }
+  return formatString;
+};
+
+
+
+module.exports = {
+  formatTime,
+  request,
+  redirect,
+  showErrorToast,
+  showSuccessToast,
+  checkSession,
+  login,
+  formateDate,
+}
