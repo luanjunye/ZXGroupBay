038bc55bf82ed98f34513bfafe2d9ff3dac071bd
KyleBing <kylebing@163.com>
2019-11-21 15:06:54 +0800

    反馈提交页面修改中，放置

--- pages/userCenter/feedbackApply/feedbackApply.js
+++ pages/userCenter/feedbackApply/feedbackApply.js
@@ -2,24 +2,27 @@ const util = require('../../../utils/util');
 
 Page({
   data: {
-    // navbar
-    navbarTabs: [
-      { id:0, title: '全部', active: true},
-      { id:1, title: '待审核', active: false},
-      { id:2, title: '处理中', active: false},
-      { id:3, title: '已完成', active: false},
-    ],
 
-    feedbackList: [
-      {id: 1, title: '果冻橙申请售后', date: '2019-11-21 13:24:23', state: 1},
-      {id: 2, title: '蓝莓申请退款', date: '2019-11-21 13:24:23', state: 2},
-      {id: 3, title: '玉米理赔', date: '2019-11-21 13:24:23', state: 3},
-      {id: 4, title: '蓝莓申请退款', date: '2019-11-21 13:24:23', state: 2},
-      {id: 5, title: '蓝莓申请退款', date: '2019-11-21 13:24:23', state: 2},
-      {id: 6, title: '果冻橙申请售后', date: '2019-11-21 13:24:23', state: 1},
-      {id: 7, title: '果冻橙申请售后', date: '2019-11-21 13:24:23', state: 1},
-    ]
-
+    detailData: {
+      productPrice: 15.3,
+      returnPrice: 15.3,
+      stateId: 1,
+      state:'已解决',
+      type: '质量问题',
+      ticketNo: 56283481614,
+      productId: 1536,
+      orderId: 7123568476,
+      nickName: '飞鱼',
+      applyDateTime: '2019-11-21 11:23:12',
+      applyCount: 1,
+      title: '果冻橙果申请退款',
+      description: '物品损坏，物品已退回公司，申请退款',
+      evidences: [
+        '/assets/list1.jpg',
+        '/assets/list1.jpg',
+        '/assets/list1.jpg',
+      ]
+    }
   },
 
   onLoad: function (options) {
