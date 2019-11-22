aabf7f41fc13f93e4265db6984bbb0fb9e9eaec6
KyleBing <kylebing@163.com>
2019-11-22 15:43:49 +0800

    完成团员列表

--- pages/userCenter/memberManage/memberManage.js
+++ pages/userCenter/memberManage/memberManage.js
@@ -2,7 +2,15 @@ const util = require('../../../utils/util');
 
 Page({
   data: {
-
+    members: [
+      {id: 1, headPic: '/assets/avater.png', name: 'FlyFish', wxId: 'b_kyle', amount: '762.2'},
+      {id: 2, headPic: '/assets/avater.png', name: '掏下水道的猴', wxId: 'b_kyle', amount: '123.2'},
+      {id: 3, headPic: '/assets/avater.png', name: '掏下水道的猴', wxId: 'b_kyle', amount: '123.2'},
+      {id: 4, headPic: '/assets/avater.png', name: '掏下水道的猴', wxId: 'b_kyle', amount: '123.2'},
+      {id: 5, headPic: '/assets/avater.png', name: '掏下水道的猴', wxId: 'b_kyle', amount: '123.2'},
+      {id: 6, headPic: '/assets/avater.png', name: '掏下水道的猴', wxId: 'b_kyle', amount: '123.2'},
+      {id: 7, headPic: '/assets/avater.png', name: '掏下水道的猴', wxId: 'b_kyle', amount: '123.2'},
+    ]
   },
 
   onLoad: function (options) {
@@ -12,6 +20,13 @@ Page({
 // ========================
   onPullDownRefresh: function () {
     wx.stopPullDownRefresh()
+  },
+
+  memberTaped(e){
+    let memberId = e.currentTarget.dataset.id;
+    wx.navigateTo({
+      url: '/pages/userCenter/memberDetail/memberDetail?memberId=' + memberId
+    })
   },
 
   onReady: function () { },
