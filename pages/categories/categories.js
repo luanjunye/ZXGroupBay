14049ed0812b00ff9d4bf0ef17192fa8bc9e8e0a
KyleBing <kylebing@163.com>
2019-11-27 11:09:01 +0800

    完成分类页-载入类别，载入对应类别商品列表

--- pages/categories/categories.js
+++ pages/categories/categories.js
@@ -12,18 +12,7 @@ Page({
     count: 0,
     cartTabIndex: 2,
     categories: [],
-    goods: [
-      {
-        id: 1,
-        pic: '/assets/test/goods-4.jpg',
-        // state: 'sellOut',
-        state: 'sell',
-        name: "鲜冻小河虾",
-        type: '中国 1盒 220克',
-        priceOrigin: 1945,
-        price: 409.9
-      },
-    ]
+    goods: []
   },
 
   onLoad: function (options) {
@@ -45,50 +34,46 @@ Page({
       })
     })
 
-    // 载入当前类别的商品列表
-    util.request(api.GoodsList, {
-      "categoryId": 1,
-      "isLike": 1,
-      "keyWord": "string"
-    }, 'GET').then(res => {
-      let initMenuItem = {id: 0, name: '猜您喜欢', active: true};
-      let tempArray = [initMenuItem];
-      res.forEach(item => {
-        tempArray.push({
-          id: item.id,
-          name: item.name,
-          active: false
-        })
-      })
-      that.setData({
-        categories: tempArray
-      })
-    })
-
-
+    this.getGoodsListOf(0); // 载入【猜你喜欢】类别的商品列表
 
     // INIT screenHeight
     this.setData({
       windowHeight: app.globalData.windowHeight
     })
-
   },
+
 
   // 切换分类
   switchCategory(e){
     let clickedCategoryId = e.target.dataset.id;
     let tempArray = [];
+    this.setData({
+    })
     this.data.categories.forEach(item => {
       tempArray.push({
         id: item.id,
         name: item.name,
-        active: clickedCategoryId === item.id? true : false
+        active: clickedCategoryId === item.id
       })
     })
-
     this.setData({
+      goods: [],
       categories: tempArray,
-      [`goods[0].state`]: this.data.goods[0].state === 'sellOut'? 'sell': 'sellOut'
+    })
+    this.getGoodsListOf(clickedCategoryId);
+  },
+
+  // 载入对应类别的商品列表
+  getGoodsListOf(categoryId){
+    let that = this;
+    util.request(api.GoodsList, {
+      "categoryId": categoryId,
+      "isLike": categoryId === 0? 1: 0, // category 为 0 时，是【猜你喜欢】类别
+      "keyWord": ""
+    }, 'GET').then(res => {
+      that.setData({
+        goods: that.data.goods.concat(res.list)
+      })
     })
   },
 
