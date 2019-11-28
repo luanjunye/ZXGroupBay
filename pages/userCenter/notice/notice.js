9cdf33805ed5700840dc53bbaf5413245c867fac
Muse丶 <554953278@qq.com>
2019-11-28 16:22:27 +0800

    新增团购订单页面 新增通知页面 新增历史开团页面

---
+++ pages/userCenter/notice/notice.js
@@ -0,0 +1,105 @@
+// pages/userCenter/notice/notice.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    notice_list:[
+      {
+        id:1,
+        url: "/assets/avater.png",
+        title:"参团成功",
+        text:"您已成功参团，我们将尽快为您发货，感谢您的支持"
+      },
+      {
+        id:2,
+        url: "/assets/avater.png",
+        title:"参团成功",
+        text:"您已成功参团，我们将尽快为您发货，感谢您的支持"
+      },
+      {
+        id:3,
+        url: "/assets/avater.png",
+        title:"参团成功",
+        text:"您已成功参团，我们将尽快为您发货，感谢您的支持"
+      },
+      {
+        id:4,
+        url: "/assets/avater.png",
+        title:"参团成功",
+        text:"您已成功参团，我们将尽快为您发货，感谢您的支持"
+      },
+      {
+        id:5,
+        url: "/assets/avater.png",
+        title:"参团成功",
+        text:"您已成功参团，我们将尽快为您发货，感谢您的支持"
+      },
+      {
+        id:6,
+        url: "/assets/avater.png",
+        title:"参团成功",
+        text:"您已成功参团，我们将尽快为您发货，感谢您的支持"
+      }
+    ]
+
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
