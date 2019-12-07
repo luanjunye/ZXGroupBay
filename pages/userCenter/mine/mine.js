8bb53daf51659f1f2f89fec1c078f34106b1f571
KyleBing <kylebing@163.com>
2019-12-07 10:30:08 +0800

    未登录时显示默认用户头像，用户中心相关按钮提示需要登录才能使用，部分菜单可以直接进入，如：关于页面

--- pages/userCenter/mine/mine.js
+++ pages/userCenter/mine/mine.js
@@ -4,75 +4,86 @@ const api = require('../../../config/url.js');
 Page({
   data: {
     videoAdded: false,
-    groupMaster: true,  // 是否为团长
+    groupMaster: false,  // 是否为团长
     messageCount: 0,
     userInfo: {
-/*      avatar: "https://wx.qlogo.cn/mmopen/vi_32/AIdAmibzdhn40DjpvD3Tce9ZCbZkO3VLrRFfItR8uquB7PAJDH1yuMCNicJJtsbkVJUuKVmFLZ7v3oVaicDmeJlXw/132",
-      nickname: "十月",
-      code: "778697298",
-      amountMoney: 100,
+      avatar: "/assets/mine/nouser.png",
+      nickname: "点此处前往登录",
+      code: "",
+      amountMoney: 0,
       isRegimental: 0,  // 1=是团长 0=团员
       awaitMoney: 0,
-      predictMoney: 168.96,
-      orderCount: 33,
-      allOrderMoney: 2112*/
+      predictMoney: 0,
+      orderCount: 0,
+      allOrderMoney: 0
     },
+
+    /*
+    * 数据结构说明：
+    * -------------
+    * id: id
+    * title: 菜单名字
+    * guest: 在未登录的时候是否可点击进入
+    * type: 点击菜单时的动作类型： page: 跳转页面   method: 调用方法
+    * url: 要跳转的页面名字|要执行的方法名
+    * iconUrl: 菜单图标
+    * badge: 图标的角标数字
+    * */
 
     // 普通用户菜单组
     menuListNormal: [
-      {id: 0, title: '待付款', type: 'page',   url: '/pages/order/orderCenter/orderCenter?tab=1' ,iconUrl: '/assets/mine/tab-unpaid.png', badge: 0},
-      {id: 1, title: '待收货', type: 'page',   url: '/pages/order/orderCenter/orderCenter?tab=2' ,iconUrl: '/assets/mine/tab-receive.png', badge: 0},
-      {id: 2, title: '已完成', type: 'page',   url: '/pages/order/orderCenter/orderCenter?tab=3' ,iconUrl: '/assets/mine/tab-finished.png', badge: 0},
-      {id: 3, title: '退款售后', type: 'page', url: '/pages/order/orderCenter/orderCenter?tab=4' ,iconUrl: '/assets/mine/tab-service.png', badge: 0},
+      {id: 0, title: '待付款', guest: false, type: 'page',   url: '/pages/order/orderCenter/orderCenter?tab=1' ,iconUrl: '/assets/mine/tab-unpaid.png', badge: 0},
+      {id: 1, title: '待收货', guest: false, type: 'page',   url: '/pages/order/orderCenter/orderCenter?tab=2' ,iconUrl: '/assets/mine/tab-receive.png', badge: 0},
+      {id: 2, title: '已完成', guest: false, type: 'page',   url: '/pages/order/orderCenter/orderCenter?tab=3' ,iconUrl: '/assets/mine/tab-finished.png', badge: 0},
+      {id: 3, title: '退款售后', guest: false, type: 'page', url: '/pages/order/orderCenter/orderCenter?tab=4' ,iconUrl: '/assets/mine/tab-service.png', badge: 0},
     ],
 
     // 我的团购菜单组
     menuListGroup: [
-      {id: 0, title: '团购订单', type: 'page', url: '/pages/userCenter/groupOrder/groupOrder?from=mine' ,iconUrl: '/assets/mine/load-cargo.png', badge: 0},
-      {id: 1, title: '签收码',   type: 'method', url: 'showDeveloping' ,iconUrl: '/assets/mine/receive-code.png', badge: 0},
-      {id: 2, title: '售后反馈', type: 'page', url: '/pages/userCenter/feedback/feedback' ,iconUrl: '/assets/mine/feedback.png', badge: 0},
-      {id: 3, title: '佣金记录', type: 'page', url: '/pages/userCenter/commission/commission' ,iconUrl: '/assets/mine/commission.png', badge: 0},
-      {id: 4, title: '团员管理', type: 'page', url: '/pages/userCenter/memberManage/memberManage' ,iconUrl: '/assets/mine/member-manage.png', badge: 0},
-      {id: 5, title: '历史开团', type: 'page', url: '/pages/userCenter/historyGroup/historyGroup' ,iconUrl: '/assets/mine/tuan-history.png', badge: 0},
+      {id: 0, title: '团购订单', guest: false, type: 'page', url: '/pages/userCenter/groupOrder/groupOrder?from=mine' ,iconUrl: '/assets/mine/load-cargo.png', badge: 0},
+      {id: 1, title: '签收码',   guest: false, type: 'method', url: 'showDeveloping' ,iconUrl: '/assets/mine/receive-code.png', badge: 0},
+      {id: 2, title: '售后反馈', guest: false, type: 'page', url: '/pages/userCenter/feedback/feedback' ,iconUrl: '/assets/mine/feedback.png', badge: 0},
+      {id: 3, title: '佣金记录', guest: false, type: 'page', url: '/pages/userCenter/commission/commission' ,iconUrl: '/assets/mine/commission.png', badge: 0},
+      {id: 4, title: '团员管理', guest: false, type: 'page', url: '/pages/userCenter/memberManage/memberManage' ,iconUrl: '/assets/mine/member-manage.png', badge: 0},
+      {id: 5, title: '历史开团', guest: false, type: 'page', url: '/pages/userCenter/historyGroup/historyGroup' ,iconUrl: '/assets/mine/tuan-history.png', badge: 0},
     ],
 
     // 营销工具菜单组
     menuListSell: [
-      {id: 0, title: '开团海报', type: 'page',   url: '/pages/userCenter/poster/poster' ,iconUrl: '/assets/mine/poster.png', badge: 0},
-      {id: 1, title: '优惠券包', type: 'method', url: 'showDeveloping' ,iconUrl: '/assets/mine/coupon.png', badge: 0},
-      {id: 2, title: '优惠活动', type: 'method', url: 'showDiscountActivity' ,iconUrl: '/assets/mine/gift.png', badge: 0},
+      {id: 0, title: '开团海报', guest: false, type: 'page',   url: '/pages/userCenter/poster/poster' ,iconUrl: '/assets/mine/poster.png', badge: 0},
+      {id: 1, title: '优惠券包', guest: false, type: 'method', url: 'showDeveloping' ,iconUrl: '/assets/mine/coupon.png', badge: 0},
+      {id: 2, title: '优惠活动', guest: false, type: 'method', url: 'showDiscountActivity' ,iconUrl: '/assets/mine/gift.png', badge: 0},
     ],
 
     // 普通会员菜单
-    // 申请团长菜单项，单独在列出来
     menuNormal: [
-      {id: 0, title: '我要成为团长', type: 'method',  url: 'applyGroupMaster' ,iconUrl: '/assets/mine/icon-submit.png', badge: 0},
-      {id: 1, title: '优惠券',      type: 'method',  url: 'showDeveloping' ,iconUrl: '/assets/mine/icon-coupon.png', badge: 0},
-      {id: 2, title: '更换团长',    type: 'page',    url: '/pages/regimentalCommander/changeCommander' ,iconUrl: '/assets/mine/icon-switch.png', badge: 0},
-      {id: 3, title: '关于泽轩优选', type: 'page',    url: '/pages/userCenter/about/about' ,iconUrl: '/assets/mine/icon-about.png', badge: 0},
-      // {id: 4, title: '视频播放', type: 'page',    url: '/pages/video/video' ,iconUrl: '/assets/mine/icon-about.png', badge: 0},
+      {id: 0, title: '我要成为团长', guest: false, type: 'method',  url: 'applyGroupMaster' ,iconUrl: '/assets/mine/icon-submit.png', badge: 0},
+      {id: 1, title: '优惠券',      guest: false, type: 'method',  url: 'showDeveloping' ,iconUrl: '/assets/mine/icon-coupon.png', badge: 0},
+      {id: 2, title: '更换团长',    guest: false, type: 'page',    url: '/pages/regimentalCommander/changeCommander' ,iconUrl: '/assets/mine/icon-switch.png', badge: 0},
+      {id: 3, title: '关于泽轩优选', guest: true, type: 'page',    url: '/pages/userCenter/about/about' ,iconUrl: '/assets/mine/icon-about.png', badge: 0},
+      // {id: 4, title: '视频播放', guest: false, type: 'page',    url: '/pages/video/video' ,iconUrl: '/assets/mine/icon-about.png', badge: 0},
     ],
 
     // 团长菜单
     menuMaster: [
-      {id: 0, title: '优惠券',         type: 'method', url: 'showDeveloping' ,iconUrl: '/assets/mine/icon-coupon.png', badge: 0},
-      {id: 1, title: '邀请好友注册团长', type: 'method', url: 'showDeveloping' ,iconUrl: '/assets/mine/icon-invitation.png', badge: 0},
-      // {id: 1, title: '邀请好友注册团长', type: 'page', url: '/pages/userCenter/invitation/invitation' ,iconUrl: '/assets/mine/icon-invitation.png', badge: 0},
-      {id: 2, title: '关于泽轩优选',    type: 'page', url: '/pages/userCenter/about/about' ,iconUrl: '/assets/mine/icon-about.png', badge: 0},
+      {id: 0, title: '优惠券',         guest: false, type: 'method', url: 'showDeveloping' ,iconUrl: '/assets/mine/icon-coupon.png', badge: 0},
+      {id: 1, title: '邀请好友注册团长', guest: false, type: 'method', url: 'showDeveloping' ,iconUrl: '/assets/mine/icon-invitation.png', badge: 0},
+      // {id: 1, title: '邀请好友注册团长', guest: false, type: 'page', url: '/pages/userCenter/invitation/invitation' ,iconUrl: '/assets/mine/icon-invitation.png', badge: 0},
+      {id: 2, title: '关于泽轩优选',    guest: true, type: 'page', url: '/pages/userCenter/about/about' ,iconUrl: '/assets/mine/icon-about.png', badge: 0},
     ],
   },
 
 
-
-
-  onLoad: function (options) {
-    this.getUserInformation();
+  onLoad: function (options) {},
+  onShow: function () {
+    if(util.getUserInfo().userId){  // 只有登录后才请求用户数据
+      this.getUserInformation();
+    }
     util.updateCartCount(); // 更新购物车图标 badge
   },
 
   // 申请团长点击时
   applyGroupMaster(){
-
     util.request(api.MasterStateCheck, {
       userId: util.getUserInfo().userId
     }, "POST").then(res => {
@@ -90,6 +101,14 @@ Page({
     })
   },
 
+  // 前往登录
+  login(){
+    if(!util.getUserInfo().userId){
+      wx.navigateTo({
+        url: '/pages/login/login'
+      })
+    }
+  },
 
 
   // 获取用户信息
@@ -97,7 +116,7 @@ Page({
     let that = this;
     util.request(api.UserInfo, {
       userId: util.getUserInfo().userId
-    }, "POST").then(res => {
+    }, "POST", true).then(res => {
       wx.setStorageSync('isMaster', Boolean(res.isRegimental)); // 更新 storage 用户状态：是否为团长
       that.setData({
         userInfo: res,
@@ -111,12 +130,19 @@ Page({
   switchMenu(e){
     let pageUrl = e.currentTarget.dataset.url;
     let type = e.currentTarget.dataset.type;
-    if (type === 'page'){ // 跳转页面时
-      wx.navigateTo({
-        url: pageUrl
-      })
-    } else {  // 调用方法时
-      this[pageUrl]()
+    let guest = e.currentTarget.dataset.guest;
+
+    // 未登录 或者  菜单显示为 guest 可点击时，生效
+    if(util.getUserInfo().userId || guest){
+      if (type === 'page'){ // 跳转页面时
+        wx.navigateTo({
+          url: pageUrl
+        })
+      } else {  // 调用方法时
+        this[pageUrl]()
+      }
+    } else {
+      util.toast('您当前未登录');
     }
   },
 
@@ -135,13 +161,11 @@ Page({
     })
   },
 
-  //进入通知
-  toNotice:function(){
-    wx.navigateTo({
-      url: "/pages/userCenter/notice/notice"
-    })
-  },
 
+
+  /* *******
+  *  菜单相关
+  * ********/
 
   // 添加视频播放菜单
   showVideo(){
@@ -167,13 +191,16 @@ Page({
   
 // ========================
   onPullDownRefresh: function () {
-    this.getUserInformation();
+    if(util.getUserInfo().userId){  // 只有登录后才请求用户数据
+      this.getUserInformation();
+    } else {
+      wx.stopPullDownRefresh();
+    }
     this.updateVideoStatus();
   },
 
   onReady: function () { },
-  onShow: function () {
-  },
+
 
   // onHide: function () { },
   // onUnload: function () { },
