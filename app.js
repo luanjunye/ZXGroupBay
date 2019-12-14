9b9efc0114e96d977c5c9dd89c2a1bd2a5949dbc
KyleBing <kylebing@163.com>
2019-12-14 14:53:48 +0800

    添加小程序版本自动更新 我的页面添加版本号显示

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const util = require('/utils/util.js');
 
 App({
   onLaunch: function () {
-
     wx.getSystemInfo({
       success: res => {
         this.globalData.safeTop = res.safeArea.top;
@@ -49,6 +48,39 @@ App({
     //     }
     //   }
     // })
+
+
+    /**
+     * 版本自动更新
+     * @type {wx.UpdateManager}
+     */
+    const updateManager = wx.getUpdateManager()
+
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调
+      console.log(res.hasUpdate)
+    })
+
+/*    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success: function (res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    }) */
+
+    updateManager.onUpdateReady(function () {
+      updateManager.applyUpdate() // 检测到更新，直接更新
+    })
+
+    updateManager.onUpdateFailed(function () {
+      // 新版本下载失败
+    })
   },
   globalData: {
     safeTop:      Number,
