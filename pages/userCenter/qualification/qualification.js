515f50fdc3dfa6e7c93fc1e94142da0ad81508b1
KyleBing <kylebing@163.com>
2019-12-17 15:42:19 +0800

    添加资质信息页面

---
+++ pages/userCenter/qualification/qualification.js
@@ -0,0 +1,23 @@
+const util = require('../../../utils/util');
+
+Page({
+  data: {
+    tapCount: 0,
+  },
+
+  onLoad: function (options) {},
+
+
+// ========================
+  onPullDownRefresh: function () {
+    wx.stopPullDownRefresh()
+  },
+
+  onReady: function () { },
+  onShow: function () { },
+
+  // onHide: function () { },
+  // onUnload: function () { },
+  // onReachBottom: function () { },
+  // onShareAppMessage: function () { }
+});
