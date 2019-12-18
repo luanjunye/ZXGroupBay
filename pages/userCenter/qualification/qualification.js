41949538cacac792420d9bd2b1b686b99243df0b
KyleBing <kylebing@163.com>
2019-12-18 16:49:35 +0800

    提现限制10元以上，并提示10元冻结款

--- pages/userCenter/qualification/qualification.js
+++ pages/userCenter/qualification/qualification.js
@@ -2,7 +2,11 @@ const util = require('../../../utils/util');
 
 Page({
   data: {
-    tapCount: 0,
+    pics: [
+      'https://xiaochengxu-tuan.oss-cn-hangzhou.aliyuncs.com/images/2019-12-18/157665840556892.jpg',
+      'https://xiaochengxu-tuan.oss-cn-hangzhou.aliyuncs.com/images/2019-12-18/157665841857795.jpg',
+      'https://xiaochengxu-tuan.oss-cn-hangzhou.aliyuncs.com/images/2019-12-18/157665842427284.jpg'
+      ]
   },
 
   onLoad: function (options) {},
